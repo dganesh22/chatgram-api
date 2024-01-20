bf7f568901265f5224dca8058a148ad05666371d
dganesh22 <dganesh2022@gmail.com>
2024-01-20 20:10:37 +0530

    updated server.js

--- server.js
+++ server.js
@@ -30,9 +30,10 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
+  app.get("*", (req, res) => {
+    // res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    return res.status(404).json({ msg: `Hi welcome to chatgaram-api..`})
+});
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
